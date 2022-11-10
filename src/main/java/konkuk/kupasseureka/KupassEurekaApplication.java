cb35f638a2a88298a225897fd24a7b48d3b18858
kyuhyeongkwon <akcps1tp@naver.com>
2022-11-10 14:31:48 +0900

    feat: 유레카 서버 메인

---
+++ src/main/java/konkuk/kupasseureka/KupassEurekaApplication.java
@@ -0,0 +1,15 @@
+package konkuk.kupasseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class KupassEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KupassEurekaApplication.class, args);
+	}
+
+}
